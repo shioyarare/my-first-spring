210f78d5069a666429cfa9ce2ac177f5f16fd723
Shoya HARA <admin@ml.harashio.jp>
2022-08-23 03:15:30 +0900

    feat: return json

--- src/main/java/com/example/restservice/posts/PostsController.java
+++ src/main/java/com/example/restservice/posts/PostsController.java
@@ -1,29 +1,51 @@
 package com.example.restservice.posts;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.Media;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 public class PostsController {
-    @GetMapping("/posts")
-    public String indexPost() {
-       return "get /posts";
+    HttpHeaders headers = new HttpHeaders();
+    PostsController() {
+        headers.setContentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8));
     }
 
+    // get index
+    @GetMapping("/posts")
+    @JsonValue
+    public ResponseEntity<Map> indexPost() {
+        Map<String, Object> map = new LinkedHashMap<>();
+        return new ResponseEntity<Map>(map, headers, HttpStatus.OK);
+    }
+
+    // get show
     @RequestMapping(value = "/posts/{postId}", method = RequestMethod.GET)
     public String showPost(@PathVariable("postId") String postId) {
        return "get /posts id = " + postId;
     }
 
+    // post
     @PostMapping("/posts")
     public String setPost() {
         return "post /posts";
     }
 
+    // put
     @RequestMapping(value = "/posts/{postId}", method = RequestMethod.PUT)
     public String updatePost() {
         return "put /posts";
     }
 
+    // delete
     @DeleteMapping("/posts/{postId}")
     public String deletePost(@PathVariable("postId") String postId) {
         return "delete /posts id = " + postId;
