d0d426bc3b145a95d8bbaa7cf44f8307940a30cf
Shoya HARA <admin@ml.harashio.jp>
2022-08-23 02:04:45 +0900

    feat: showはこうやってルートを追加できそう

--- src/main/java/com/example/restservice/posts/PostsController.java
+++ src/main/java/com/example/restservice/posts/PostsController.java
@@ -1,14 +1,17 @@
 package com.example.restservice.posts;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PostsController {
     @GetMapping("/posts")
-    public String getAllPosts() {
+    public String indexPost() {
        return "get /posts";
+    }
+
+    @RequestMapping("/posts/{postId}")
+    public String showPost(@PathVariable("postId") String postId) {
+       return "get /posts id = " + postId;
     }
 
     @PostMapping("/posts")
